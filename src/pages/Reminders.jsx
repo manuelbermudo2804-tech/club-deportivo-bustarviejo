1e92faff4be67d98ec5d3ded586f45d3fc75e8d0
Manuel Bermudo Santacruz <manuelbermudo2804@gmail.com>
2025-11-19 08:00:18 +0000

    File changes

--- src/pages/Reminders.jsx
+++ src/pages/Reminders.jsx
@@ -1012,8 +1012,8 @@ Temporada ${reminder.temporada}
                                   </div>
                                 ) : (
                                   <div className="w-5 h-5 flex items-center justify-center flex-shrink-0">
-                                    <svg className="w-4 h-4 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                    <svg className="w-5 h-5 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M6 18L18 6M6 6l12 12" />
                                     </svg>
                                   </div>
                                 )}
