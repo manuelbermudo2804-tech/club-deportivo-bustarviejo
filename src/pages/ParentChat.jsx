27f8e1fe90ab46b95500b79f742038e84e873b0c
Manuel Bermudo Santacruz <manuelbermudo2804@gmail.com>
2025-11-25 11:39:34 +0000

    File changes

--- src/pages/ParentChat.jsx
+++ src/pages/ParentChat.jsx
@@ -285,7 +285,7 @@ export default function ParentChat() {
         existingConversations={privateConversations}
         onConversationCreated={handleConversationCreated}
       />
-    <div className="p-4 lg:p-6 min-h-screen bg-slate-50">
+      <div className="p-4 lg:p-6 min-h-screen bg-slate-50">
       {/* Header con tabs */}
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4 mb-4">
         <div>
