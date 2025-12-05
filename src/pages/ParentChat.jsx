9a666759ec8d03179bd78b9a7df650f51a313cc9
Manuel Bermudo Santacruz <manuelbermudo2804@gmail.com>
2025-12-05 14:18:45 +0000

    File changes

--- src/pages/ParentChat.jsx
+++ src/pages/ParentChat.jsx
@@ -520,6 +520,7 @@ export default function ParentChat() {
                             </div>
                           </div>
                         ))}
+                      <div ref={coordinationMessagesEndRef} />
                     </div>
                   </div>
                 )}
