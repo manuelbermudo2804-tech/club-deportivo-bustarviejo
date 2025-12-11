59f893f53991c7f9250449b359bcc1f53c9a4b7e
Manuel Bermudo Santacruz <manuelbermudo2804@gmail.com>
2025-12-11 07:36:53 +0000

    File changes

--- src/components/tutorials/useTutorial.jsx
+++ src/components/tutorials/useTutorial.jsx
@@ -14,7 +14,7 @@ export function usePageTutorial(tutorialId) {
     }, 800);
 
     return () => clearTimeout(timer);
-  }, [tutorial?.isReady, tutorialId]);
+  }, [tutorial?.isReady, tutorialId, tutorial?.checkAndStartTutorial]);
 
   return tutorial;
 }
