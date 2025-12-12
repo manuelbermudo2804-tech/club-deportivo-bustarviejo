c4232ed5c583064d32c0d35e9965e333e00f44af
Manuel Bermudo Santacruz <manuelbermudo2804@gmail.com>
2025-12-12 12:07:36 +0000

    File changes

--- src/components/messaging/FamilyChatsWithTabs.jsx
+++ src/components/messaging/FamilyChatsWithTabs.jsx
@@ -14,9 +14,9 @@ export default function FamilyChatsWithTabs({ isCoordinator, isCoach, Coordinato
   
   // Si es ambos, mostrar con tabs
   return (
-    <div className="h-[calc(100vh-100px)] lg:h-[calc(100vh-110px)]">
+    <div className="h-screen lg:h-screen">
       <Tabs defaultValue="coordinador" className="h-full flex flex-col">
-        <div className="bg-white border-b px-4 py-2 flex-shrink-0">
+        <div className="bg-white border-b px-4 py-2 flex-shrink-0 lg:mt-0 mt-[100px]">
           <TabsList className="grid w-full grid-cols-2 max-w-md mx-auto">
             <TabsTrigger value="coordinador" className="text-sm">
               🏟️ Como Coordinador
@@ -27,13 +27,17 @@ export default function FamilyChatsWithTabs({ isCoordinator, isCoach, Coordinato
           </TabsList>
         </div>
         
-        <div className="flex-1 overflow-hidden">
-          <TabsContent value="coordinador" className="h-full m-0">
-            <CoordinatorChatPage />
+        <div className="flex-1 overflow-hidden min-h-0">
+          <TabsContent value="coordinador" className="h-full m-0 data-[state=active]:flex data-[state=active]:flex-col">
+            <div className="h-full overflow-hidden">
+              <CoordinatorChatPage />
+            </div>
           </TabsContent>
           
-          <TabsContent value="entrenador" className="h-full m-0">
-            <CoachParentChatPage />
+          <TabsContent value="entrenador" className="h-full m-0 data-[state=active]:flex data-[state=active]:flex-col">
+            <div className="h-full overflow-hidden">
+              <CoachParentChatPage />
+            </div>
           </TabsContent>
         </div>
       </Tabs>
