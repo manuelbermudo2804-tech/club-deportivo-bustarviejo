78e18bd531fc2d282d39491f3e4531470f1d09db
Manuel Bermudo Santacruz <manuelbermudo2804@gmail.com>
2025-12-12 20:33:12 +0000

    File changes

--- src/pages/ParentCoachChat.jsx
+++ src/pages/ParentCoachChat.jsx
@@ -298,7 +298,7 @@ export default function ParentCoachChat() {
         </div>
       )}
 
-    <div className="h-full lg:p-4 lg:max-w-5xl lg:mx-auto overflow-hidden">
+    <div className="h-[calc(100vh-100px)] lg:h-[calc(100vh-110px)] lg:p-4 lg:max-w-5xl lg:mx-auto overflow-hidden">
       <Card className="border-blue-200 shadow-lg h-full flex flex-col overflow-hidden lg:rounded-lg rounded-none">
         <CardHeader className="bg-gradient-to-r from-blue-600 to-blue-700 text-white p-2 flex-shrink-0">
           <div className="flex items-center justify-between">
