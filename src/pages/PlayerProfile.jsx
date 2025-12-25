2b32f8b2dc5b75b1e8b5f40cd92a09e0bf85c5f7
Manuel Bermudo Santacruz <manuelbermudo2804@gmail.com>
2025-12-25 20:47:36 +0000

    File changes

--- src/pages/PlayerProfile.jsx
+++ src/pages/PlayerProfile.jsx
@@ -199,7 +199,7 @@ export default function PlayerProfile() {
         </Button>
       </div>
 
-      <div className="grid md:grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-2 gap-4 items-start">
         <Card>
           <CardHeader>
             <CardTitle className="text-base">Datos personales</CardTitle>
