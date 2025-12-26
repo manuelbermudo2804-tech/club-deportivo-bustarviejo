aa6b23a87f503ace26d8184a87c238d923effb77
Manuel Bermudo Santacruz <manuelbermudo2804@gmail.com>
2025-12-26 10:51:43 +0000

    File changes

--- src/components/standings/UploadStandingsForm.jsx
+++ src/components/standings/UploadStandingsForm.jsx
@@ -27,14 +27,20 @@ export default function UploadStandingsForm({ onDataExtracted, onCancel, presele
     if (!categoria) return;
     (async () => {
       try {
-        const configs = await base44.entities.StandingsConfig.list();
-        const cfg = configs.find(c => c.categoria === categoria);
+        const cfgs = await base44.entities.StandingsConfig.filter({ categoria });
+        const cfg = cfgs?.[0];
         if (cfg) {
           setRfefUrlState(cfg.rfef_url || "");
           setGrupoText(cfg.grupo || "");
           setConfigId(cfg.id);
+        } else {
+          setRfefUrlState("");
+          setGrupoText("");
+          setConfigId(null);
         }
-      } catch (e) {}
+      } catch (e) {
+        setConfigId(null);
+      }
     })();
   }, [categoria]);
 
